a925230382982ced7151d816ea81646481318b16
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-21 01:39:59 -0500

    formatted code, deployed to netlify, adjusted link to deployed site

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,18 +1,18 @@
 import { useState, useEffect, useCallback } from 'react';
 
 export const useGameStatus = (rowsCleared) => {
-	const [ score, setScore ] = useState(0);
-	const [ rows, setRows ] = useState(0);
-	const [ level, setLevel ] = useState(0);
+	const [score, setScore] = useState(0)
+	const [rows, setRows] = useState(0)
+	const [level, setLevel] = useState(0)
 
-	const linePoints = [ 40, 100, 300, 1200 ];
+	const linePoints = [40, 100, 300, 1200]
 
 	const calcScore = useCallback(() => {
 			// We have score
 			if (rowsCleared > 0) {
 				// This is how original Tetris score is calculated
-				setScore((prev) => prev + linePoints[rowsCleared - 1] * level);
-				setRows((prev) => prev + rowsCleared);
+				setScore((prev) => prev + linePoints[rowsCleared - 1] * level)
+				setRows((prev) => prev + rowsCleared)
 			}
 		},
 		[ level, linePoints, rowsCleared ]
@@ -22,7 +22,7 @@ export const useGameStatus = (rowsCleared) => {
 			calcScore();
 		},
 		[ calcScore, rowsCleared, score ]
-	);
+	)
 
-	return [ score, setScore, rows, setRows, level, setLevel ];
+	return [ score, setScore, rows, setRows, level, setLevel ]
 };
