2982ae80cc7ada04088660751b453eb915f04fa7
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-21 04:53:06 -0500

    did npm install react-titles, animated title works but does not appear before the game starts

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import ReactTitle from "react-titles/Title2";
+
+class Title extends Component {
+    render() {
+        return (
+            <ReactTitle size="400" text1="I LOVE" text2="REACT" open={true} />
+        );
+    }
+};
+
+export default Title
