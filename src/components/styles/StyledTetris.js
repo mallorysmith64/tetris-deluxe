e98427da6559c404c72aca6e191d7fde71580660
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-21 07:48:33 -0500

    moved game slightly

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -11,7 +11,7 @@ export const StyledTetrisWrapper = styled.div`
 export const StyledTetris = styled.div`
     display:flex;
     align-items:flex-start;
-    padding: 0px 18px 18px 450px;
+    padding: 0px 18px 18px 470px;
     margin:0 auto;
     max-width:900px;
 
