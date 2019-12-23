c741d9d6dc355f69bf474d2087fcc1ab5c53986a
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-23 13:29:41 -0500

    changed background photo, restyled stage for game, deleted title component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Title from './components/Title'
 import Tetris from './components/Tetris'
 
 class App extends Component {
   render() {
     return (
       <>
-      <Title/>
       <Tetris/>
       </>
     )
--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -2,16 +2,17 @@ import styled from 'styled-components'
 import space from '../../images/space-background.png'
 
 export const StyledTetrisWrapper = styled.div`
-    background:url(${space}) #000;
+    // background:url(${space}) #000;
+    background:url("https://ak9.picdn.net/shutterstock/videos/18681329/thumb/1.jpg");
     background-size:cover;
     overflow:hidden;
-    height:35em;
+    height:41.6em;
 `;
 
 export const StyledTetris = styled.div`
     display:flex;
     align-items:flex-start;
-    padding: 0px 18px 18px 470px;
+    padding: 55px 18px 18px 470px;
     margin:0 auto;
     max-width:900px;
 
