89ba579b18a997137f35572dd6cf1f3034feb0fc
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-23 14:47:03 -0500

    made audio loop

--- src/audioPlayer/Audio.js
+++ src/audioPlayer/Audio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Tetris from './Tetris_Theme_Song.webm'
 
 const Audio = () => (
-  <audio src={Tetris} autoplay="true"/>
+  <audio src={Tetris} autoplay="true" loop/>
   );
 
   export default Audio
